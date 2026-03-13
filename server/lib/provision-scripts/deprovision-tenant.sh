34138a1715ca7d077db536913fa0626bc74f45f6
Hoseong-Seo <hosseo@amazon.com>
2026-03-13 13:38:49 +0900

    Tar log removed

--- server/lib/provision-scripts/deprovision-tenant.sh
+++ server/lib/provision-scripts/deprovision-tenant.sh
@@ -170,7 +170,7 @@ if [[ $TIER == "PREMIUM" || $TIER == "ADVANCED" ]]; then
   CDK_PARAM_COMMIT_ID=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='S3SourceVersion'].OutputValue" --output text)
 
   aws s3api get-object --bucket "$CDK_PARAM_S3_BUCKET_NAME" --key "$CDK_SOURCE_NAME" --version-id "$CDK_PARAM_COMMIT_ID" "$CDK_SOURCE_NAME" 2>&1 
-  tar -xzf $CDK_SOURCE_NAME
+  tar --warning=no-unknown-keyword -xzf $CDK_SOURCE_NAME 2>/dev/null || tar -xzf $CDK_SOURCE_NAME
   cd ./server
 
   sed "s/<REGION>/$REGION/g; s/<ACCOUNT_ID>/$ACCOUNT_ID/g" ./service-info.txt > ./lib/service-info.json
